eea0261982decf1d606523c99c9adc1a1b68da64
Andreas Schreiner <andreas.schreiner@uplink.systems>
2026-04-16 10:53:10 +0200

    update content for release v1.0.0

---
+++ main.tf
@@ -0,0 +1,67 @@
+####################################################################################################
+#   main.tf                                                                                        #
+####################################################################################################
+
+# generate password
+resource "random_password" "password" {
+  length                = var.user.is_admin == false ? 12 : 16
+  special               = true
+  min_lower             = 1
+  min_numeric           = 1
+  min_special           = 1
+  min_upper             = 1
+  override_special      = "!#$%&*()-_=+[]{}<>:?"
+}
+
+# create user
+resource "azuread_user" "user" {
+  surname                     = var.user.surname
+  given_name                  = var.user.given_name
+  display_name                = var.user.display_name == null ? local.display_name : var.user.display_name
+  user_principal_name         = var.user.user_principal_name == null ? local.user_principal_name : var.user.user_principal_name
+  account_enabled             = var.user.account_enabled
+  password                    = random_password.password.result
+  force_password_change       = var.user.force_password_change
+  disable_password_expiration = var.user.disable_password_expiration
+  disable_strong_password     = var.user.is_admin == false ? var.user.disable_strong_password : false
+  preferred_language          = var.user.preferred_language
+  usage_location              = var.user.usage_location
+  mail_nickname               = var.user.mail_nickname == null ? local.mail_nickname : var.user.mail_nickname
+  mail                        = var.user.mail == null ? local.mail : var.user.mail
+  other_mails                 = var.user.other_mails
+  show_in_address_list        = var.user.is_admin == false ? var.user.show_in_address_list : false
+  employee_id                 = var.user.employee_id
+  employee_type               = var.user.employee_type
+  job_title                   = var.user.job_title
+  company_name                = var.user.company_name
+  division                    = var.user.division
+  department                  = var.user.department
+  cost_center                 = var.user.cost_center
+  manager_id                  = var.user.is_admin == false ? (var.user.manager_id == null ? null : data.azuread_user.manager_id.0.object_id) : null
+  country                     = var.user.country
+  state                       = var.user.state
+  postal_code                 = var.user.postal_code
+  city                        = var.user.city
+  street_address              = var.user.street_address
+  office_location             = var.user.office_location
+  business_phones             = var.user.business_phones
+  mobile_phone                = var.user.mobile_phone
+  fax_number                  = var.user.fax_number
+  age_group                   = var.user.parental_control.age_group
+  consent_provided_for_minor  = var.user.parental_control.consent_provided_for_minor
+  depends_on                  = [ random_password.password ]
+  lifecycle {
+    ignore_changes  = [ force_password_change, password, preferred_language, usage_location ]
+  }
+}
+
+# create credential output file
+resource "local_sensitive_file" "credential" {
+  count       = var.user.export.enabled == true ? 1 : 0
+  content     = <<-EOT
+    ${azuread_user.user.user_principal_name}
+    ${nonsensitive(random_password.password.result)}
+  EOT
+  filename    = local.export.filename
+  depends_on  = [ azuread_user.user ]
+}
---
+++ variable.tf
@@ -0,0 +1,132 @@
+####################################################################################################
+#   variables.tf                                                                                   #
+####################################################################################################
+
+variable "user" {
+  type = object({
+    given_name                  = string
+    surname                     = string
+    display_name                = optional(string, null)
+    user_principal_name         = optional(string, null)
+    account_enabled             = optional(bool, true)
+    force_password_change       = optional(bool, true)
+    disable_password_expiration = optional(bool, false)
+    disable_strong_password     = optional(bool, false)
+    is_admin                    = optional(bool, false)
+    preferred_language          = optional(string, "en-US")
+    usage_location              = optional(string, null)
+    mail_nickname               = optional(string, null)
+    mail                        = optional(string, null)
+    other_mails                 = optional(list(string), [])
+    show_in_address_list        = optional(bool, true)
+    employee_id                 = optional(string, null)
+    employee_type               = optional(string, null)
+    job_title                   = optional(string, null)
+    company_name                = optional(string, null)
+    division                    = optional(string, null)
+    department                  = optional(string, null)
+    cost_center                 = optional(string, null)
+    manager_id                  = optional(string, null)
+    sponsors                    = optional(list(string), [])
+    country                     = optional(string, null)
+    state                       = optional(string, null)
+    postal_code                 = optional(string, null)
+    city                        = optional(string, null)
+    street_address              = optional(string, null)
+    office_location             = optional(string, null)
+    business_phones             = optional(list(string), [])
+    mobile_phone                = optional(string, null)
+    fax_number                  = optional(string, null)
+    parental_control            = optional(object({
+      enabled                     = optional(bool, true)
+      age_group                   = optional(string, null)
+      consent_provided_for_minor  = optional(string, null)
+    }), { enabled = false })
+    export                      = optional(object({
+      enabled                     = optional(bool, true)
+      file                        = optional(string, null)
+      path                        = optional(string, null)
+    }), { enabled = true })
+  })
+  # Validate variable attributes
+  validation {
+    condition     = var.user.user_principal_name == null ? true : can(length(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.user.user_principal_name)) > 0)
+    error_message = <<-EOF
+      Variable attribute 'user_principal_name' has an invalid value: ${var.user.user_principal_name == null ? 0 : var.user.user_principal_name}
+      Value must be one of:
+        RFC 822 based login UPN or null
+    EOF
+  }
+  validation {
+    condition     = var.user.mail == null ? true : can(length(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.user.mail)) > 0)
+    error_message = <<-EOF
+      Variable attribute 'mail' has an invalid value: ${var.user.mail == null ? 0 : var.user.mail}
+      Value must be one of:
+        RFC 5322 compliant email address or null
+    EOF
+  }
+  validation {
+    condition     = var.user.preferred_language == null ? true : can(regex("[a-z][a-z]-[A-Z][A-Z]", var.user.preferred_language))
+    error_message = <<-EOF
+      Variable attribute 'preferred_language' has an invalid value format: ${var.user.preferred_language == null ? 0 : var.user.preferred_language}
+      Value must be one of:
+        regional code "2-2" in format ([a-z][a-z]-[A-Z][A-Z]) or null
+    EOF
+  }
+  validation {
+    condition     = var.user.usage_location == null ? true : can(regex("[A-Z][A-Z]", var.user.usage_location))
+    error_message = <<-EOF
+      Variable attribute 'usage_location' has an invalid value: ${var.user.usage_location == null ? 0 : var.user.usage_location}
+      Value must be one of:
+        2-character ISO 3166-1 country code or null
+    EOF
+  }
+  validation {
+    condition     = var.user.employee_type == null ? true : contains(["Administrator", "Employee", "Contractor"], var.user.employee_type)
+    error_message = <<-EOF
+      Variable attribute 'employee_type' has an invalid value value: ${var.user.employee_type == null ? 0 : var.user.employee_type}
+      Value must be one of:
+        "Administrator", "Employee", "Contractor" or null
+    EOF
+  }
+  validation {
+    condition     = var.user.country == null ? true : can(regex("[A-Z][A-Z]", var.user.country))
+    error_message = <<-EOF
+      Variable attribute 'country' has an invalid value: ${var.user.country == null ? 0 : var.user.country}
+      Value must be one of:
+        2-character ISO 3166-1 country code or null
+    EOF
+  }
+  validation {
+    condition     = var.user.postal_code == null ? true : can(tonumber(var.user.postal_code))
+    error_message = <<-EOF
+      Variable attribute 'postal_code' has an invalid value: ${var.user.postal_code == null ? 0 : var.user.postal_code}
+      Value must be one of:
+        numerical data or null
+    EOF
+  }
+  validation {
+    condition     = var.user.manager_id == null ? true : can(length(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.user.manager_id)) > 0)
+    error_message = <<-EOF
+      Variable attribute 'manager_id' has an invalid value: ${var.user.manager_id == null ? 0 : var.user.manager_id}
+      Value must be one of:
+        RFC 822 based login UPN or null
+    EOF
+  }
+  validation {
+    condition     = var.user.parental_control.age_group == null ? true : contains(["Adult", "NotAdult", "Minor"], var.user.parental_control.age_group)
+    error_message = <<-EOF
+      Variable attribute 'parental_control.age_group' has an invalid value value: ${var.user.parental_control.age_group == null ? 0 : var.user.parental_control.age_group}
+      Value must be one of:
+        "Adult", "NotAdult", "Minor" or null
+    EOF
+  } 
+  validation {
+    condition     = var.user.parental_control.consent_provided_for_minor == null ? true : contains(["Granted", "Denied", "NotRequired"], var.user.parental_control.consent_provided_for_minor)
+    error_message = <<-EOF
+      Variable attribute 'parental_control.consent_provided_for_minor' has an invalid value: ${var.user.parental_control.consent_provided_for_minor == null ? 0 : var.user.parental_control.consent_provided_for_minor}
+      Value must be one of:
+        "Granted", "Denied", "NotRequired" or null
+    EOF
+  } 
+}
